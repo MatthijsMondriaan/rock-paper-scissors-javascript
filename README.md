8d973b33c63a866dfb8868198fe0793f1c4fe833
Matthijs <>
2023-04-05 09:18:34 +0200

    Buttons working (with double-click issue)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# rock-paper-scissors-javascript
-Rock Paper Scissors game in JavaScript
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=8009284&assignment_repo_type=AssignmentRepo)
+# JavaScript periode 4 eindtoets
+
--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 console.log("main.js loaded");
 
 // Variables
-
 let buttons = document.querySelectorAll(".button");
 
 
 // Add event listeners
-
 buttons.forEach(button => {
     button.addEventListener("click", (button) => {
         buttonClicked(button);
     });
 });
 
-
+let i = 0;
 
 // Functions
-
 function buttonClicked(button) {
     const choice = button.target.innerHTML;
-    console.log(choice);
 
+    console.log("i: " + i)
+    i++;
     switch (choice) {
         case '🪨':
             console.log("Let's rock!"); 
